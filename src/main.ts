242982cbfdf4de59b539dc8e07a5852bd444afc5
Alexandre Piel <Alexandre.Piel@zooplus.com>
2023-01-14 21:22:26 +0100

    events

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ open();
 
 setInterval(() => {
     const events = getEvents();
-    if (events.exit) {
+    if (events.exit || events.keysDown?.includes(41)) {
         close();
         exit();
     } else if (events.keysDown || events.keysUp) {
         console.log('events', events);
     }
-}, 100);
+}, 10);
